899f4c02396543db32353be06b289293182f3aa4
xiaoli <670448045@qq.com>
2025-08-20 19:30:43 +0800

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.5edd952392b63a9e88b6ca7a959f60ab.js"
+  "precache-manifest.3acc7cdb554e54ed7219d20224687daf.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "vue2"});
